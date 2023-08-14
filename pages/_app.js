6f620ae18fc8f26d577e1f76b62b9b57e4b84060
Isaac Zally Jr <izallyjr@gmail.com>
2023-08-14 21:41:38 +0000

    Adjust media queries on auth pages

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+
   return (
     <AuthProvider>
       <CustomHead />
--- pages/auth/signup.jsx
+++ pages/auth/signup.jsx
@@ -4,8 +4,8 @@ import logo from "../../images/Logo.png";
 
 function Signup() {
   return (
-    <div className="flex min-h-screen justify-center align-middle bg-slate-100">
-      <div className="bg-white shadow-ld w-2/6 p-5 text-center text-black rounded-lg my-auto">
+    <div className="min-h-screen flex justify-center align-middle bg-slate-200">
+      <div className="bg-white shadow-ld md:w-2/6 p-5 text-center text-black rounded-lg my-auto">
         <Image
           src={logo}
           width="50"
